315039e5b17b740d660cd62857534c9160fd754d
hihellohowareyou <87692784+hihellohowareyou@users.noreply.github.com>
2021-10-29 13:26:58 +0900

    updata file: delete unused

--- trainer_qa.py
+++ trainer_qa.py
@@ -112,14 +112,6 @@ class QuestionAnsweringTrainer(Trainer):
         )
         return predictions
 
-    # def compute_loss(self, model, inputs):
-    #     criterion = FocalLoss(gamma=0.5)  # 0.0 equals to CrossEntropy
-    #     labels = inputs.pop("labels")
-    #     outputs = model(**inputs)
-    #     logits = outputs[0]
-    #
-    #     return criterion(logits, labels)
-
     def compute_loss(self, model, inputs,return_outputs=False):
 
         start_positions = inputs.get("start_positions")
@@ -144,16 +136,3 @@ class QuestionAnsweringTrainer(Trainer):
         total_loss = (start_loss + end_loss) / 2
 
         return (total_loss, outputs) if return_outputs else total_loss
-
-# class multiFocalLoss(nn.modules.loss._WeightedLoss):
-#     def __init__(self, weight=None, gamma=2,reduction='mean'):
-#         super(multiFocalLoss, self).__init__(weight,reduction=reduction)
-#         self.gamma = gamma
-#         self.weight = weight #weight parameter will act as the alpha parameter to balance class weights
-#
-#     def forward(self, input, target):
-#
-#         ce_loss = F.cross_entropy(input, target,reduction=self.reduction,weight=self.weight)
-#         pt = torch.exp(-ce_loss)
-#         focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
-#         return focal_loss
