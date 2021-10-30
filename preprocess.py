e1ee00564ea00ee5a37903ca46561bd90d223342
snoop2head <young_ahn@yonsei.ac.kr>
2021-10-30 03:09:45 +0000

    fix: index movement error

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 # reference: https://github.com/snoop2head/Mathpresso_Classification/blob/main/modules/preprocess_for_kobert.py
 def preprocess(text):
-    text = re.sub(r"##|#", " ", text)  # prevent tokenizer from splitting on # or ##
     text = re.sub(r"\\r|\\n|\n|\\t", " ", text)
     text = re.sub(r"\\", " ", text)
     text = re.sub(r"\s+", " ", text)
@@ -21,3 +20,29 @@ def preprocess_json(data):
 def preprocess_df(df: pd.DataFrame):
     df["text"] = df["text"].apply(lambda x: preprocess(x))
     return df
+
+
+def preprocess_train_val(train_val: dict) -> dict:
+    """train_val such as datasets['train'] or datasets['validation'] from huggingface dataset"""
+    # fetch from train_val datasets
+    answer_start = train_val["answers"]["answer_start"][0]
+    answer_text = train_val["answers"]["text"][0]
+    context = train_val["context"]
+
+    # slice the context
+    preceding_text = context[:answer_start]
+    following_text = context[answer_start + len(answer_text) :]
+
+    # apply preprocessing for each sliced context
+    preprocessed_preceding = preprocess(preceding_text)
+    preprocessed_following = preprocess(following_text)
+
+    # make preprocessed dataset dictionary
+    new_context = preprocessed_preceding + answer_text + preprocessed_following
+    new_answer_start = answer_start - abs(len(preceding_text) - len(preprocessed_preceding))
+
+    # assign preprocessed values
+    train_val["context"] = new_context
+    train_val["answers"]["answer_start"][0] = new_answer_start
+
+    return train_val
