8cf82fb63be45afa7aae19e94e7ac694389828fe
snoop2head <young_ahn@yonsei.ac.kr>
2021-10-30 10:18:01 +0000

    return dataset, not individual preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,8 @@
 import re
 import numpy as np
 import pandas as pd
-
+from tqdm import tqdm
+from datasets import Dataset
 
 # reference: https://github.com/snoop2head/Mathpresso_Classification/blob/main/modules/preprocess_for_kobert.py
 def preprocess(text):
@@ -21,12 +22,13 @@ def preprocess_df(df: pd.DataFrame):
     return df
 
 
-def preprocess_train_val(train_val: dict) -> dict:
-    """train_val such as datasets['train'] or datasets['validation'] from huggingface dataset"""
-    # fetch from train_val datasets
-    answer_start = train_val["answers"]["answer_start"][0]
-    answer_text = train_val["answers"]["text"][0]
-    context = train_val["context"]
+def preprocess_individual_dict(single_item: dict) -> dict:
+    """preprocess individual dictionary from huggingface dataset"""
+
+    # fetch from single_item datasets
+    answer_start = single_item["answers"]["answer_start"][0]
+    answer_text = single_item["answers"]["text"][0]
+    context = single_item["context"]
 
     # slice the context
     preceding_text = context[:answer_start]
@@ -41,7 +43,18 @@ def preprocess_train_val(train_val: dict) -> dict:
     new_answer_start = answer_start - abs(len(preceding_text) - len(preprocessed_preceding))
 
     # assign preprocessed values
-    train_val["context"] = new_context
-    train_val["answers"]["answer_start"][0] = new_answer_start
+    single_item["context"] = new_context
+    single_item["answers"]["answer_start"][0] = new_answer_start
 
-    return train_val
+    return single_item
+
+
+def preprocess_train_val(train_val: dict) -> list:
+    """make preprocessed new train_val from datasets['train'] or datasets['validation'] from huggingface dataset"""
+    new_train_val = list()
+
+    for idx, single_item in tqdm(enumerate(train_val)):
+        new_train_val.append(preprocess_individual_dict(single_item))
+
+    new_train_val_dataset = Dataset.from_pandas(pd.DataFrame(new_train_val))
+    return new_train_val_dataset
