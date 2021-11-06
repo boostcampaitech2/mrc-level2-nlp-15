ce4b0681d4b6129ccda49e664c8a4284d7bb4189
kimyeondu <yjookimcd@gmail.com>
2021-11-06 22:51:14 +0900

    update ColBert

--- retrieval/colBert.py
+++ retrieval/colBert.py
@@ -181,8 +181,6 @@ def tensorize_triples(query_tokenizer, doc_tokenizer, queries, positives, negati
 
     N = len(queries)
     queries = queries.to_list()
-    # print(f'queries-----------------------------------------------------')
-    # print(queries)
     Q_ids, Q_mask = query_tokenizer.tensorize(queries)
 
     positives = positives.to_list()
@@ -232,101 +230,10 @@ def _split_into_batches(ids, mask, bsize):
     return batches
 
 
-def print_message(*s, condition=True):
-    s = ' '.join([str(x) for x in s])
-    msg = "[{}] {}".format(datetime.datetime.now().strftime("%b %d, %H:%M:%S"), s)
-
-    if condition:
-        print(msg, flush=True)
-
-    return msg
-
-
 def timestamp():
     format_str = "%Y-%m-%d_%H.%M.%S"
     result = datetime.datetime.now().strftime(format_str)
     return result
-
-
-def file_tqdm(file):
-    print(f"#> Reading {file.name}")
-
-    with tqdm.tqdm(total=os.path.getsize(file.name) / 1024.0 / 1024.0, unit="MiB") as pbar:
-        for line in file:
-            yield line
-            pbar.update(len(line) / 1024.0 / 1024.0)
-
-        pbar.close()
-
-
-def save_checkpoint(path, epoch_idx, mb_idx, model, optimizer, arguments=None):
-    print(f"#> Saving a checkpoint to {path} ..")
-
-    if hasattr(model, 'module'):
-        model = model.module  # extract model from a distributed/data-parallel wrapper
-
-    checkpoint = {}
-    checkpoint['epoch'] = epoch_idx
-    checkpoint['batch'] = mb_idx
-    checkpoint['model_state_dict'] = model.state_dict()
-    checkpoint['optimizer_state_dict'] = optimizer.state_dict()
-    checkpoint['arguments'] = arguments
-
-    torch.save(checkpoint, path)
-
-
-def load_checkpoint(path, model, optimizer=None, do_print=True):
-    if do_print:
-        print_message("#> Loading checkpoint", path, "..")
-
-    if path.startswith("http:") or path.startswith("https:"):
-        checkpoint = torch.hub.load_state_dict_from_url(path, map_location='cpu')
-    else:
-        checkpoint = torch.load(path, map_location='cpu')
-
-    state_dict = checkpoint['model_state_dict']
-    new_state_dict = OrderedDict()
-    for k, v in state_dict.items():
-        name = k
-        if k[:7] == 'module.':
-            name = k[7:]
-        new_state_dict[name] = v
-
-    checkpoint['model_state_dict'] = new_state_dict
-
-    try:
-        model.load_state_dict(checkpoint['model_state_dict'])
-    except:
-        print_message("[WARNING] Loading checkpoint with strict=False")
-        model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-
-    if optimizer:
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-
-    if do_print:
-        print_message("#> checkpoint['epoch'] =", checkpoint['epoch'])
-        print_message("#> checkpoint['batch'] =", checkpoint['batch'])
-
-    return checkpoint
-
-
-def create_directory(path):
-    if os.path.exists(path):
-        print('\n')
-        print_message("#> Note: Output directory", path, 'already exists\n\n')
-    else:
-        print('\n')
-        print_message("#> Creating directory", path, '\n\n')
-        os.makedirs(path)
-
-
-def f7(seq):
-    """
-    Source: https://stackoverflow.com/a/480227/1493011
-    """
-
-    seen = set()
-    return [x for x in seq if not (x in seen or seen.add(x))]
 
 
 def batch(group, bsize, provide_offset=False):
@@ -338,115 +245,6 @@ def batch(group, bsize, provide_offset=False):
     return
 
 
-class dotdict(dict):
-    """
-    dot.notation access to dictionary attributes
-    Credit: derek73 @ https://stackoverflow.com/questions/2352181
-    """
-    __getattr__ = dict.__getitem__
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-
-
-def flatten(L):
-    return [x for y in L for x in y]
-
-
-def zipstar(L, lazy=False):
-    """
-    A much faster A, B, C = zip(*[(a, b, c), (a, b, c), ...])
-    May return lists or tuples.
-    """
-
-    if len(L) == 0:
-        return L
-
-    width = len(L[0])
-
-    if width < 100:
-        return [[elem[idx] for elem in L] for idx in range(width)]
-
-    L = zip(*L)
-
-    return L if lazy else list(L)
-
-
-def zip_first(L1, L2):
-    length = len(L1) if type(L1) in [tuple, list] else None
-
-    L3 = list(zip(L1, L2))
-
-    assert length in [None, len(L3)], "zip_first() failure: length differs!"
-
-    return L3
-
-
-def int_or_float(val):
-    if '.' in val:
-        return float(val)
-        
-    return int(val)
-
-def load_ranking(path, types=None, lazy=False):
-    print_message(f"#> Loading the ranked lists from {path} ..")
-
-    try:
-        lists = torch.load(path)
-        lists = zipstar([l.tolist() for l in tqdm.tqdm(lists)], lazy=lazy)
-    except:
-        if types is None:
-            types = itertools.cycle([int_or_float])
-
-        with open(path) as f:
-            lists = [[typ(x) for typ, x in zip_first(types, line.strip().split('\t'))]
-                     for line in file_tqdm(f)]
-
-    return lists
-
-
-def save_ranking(ranking, path):
-    lists = zipstar(ranking)
-    lists = [torch.tensor(l) for l in lists]
-
-    torch.save(lists, path)
-
-    return lists
-
-
-def groupby_first_item(lst):
-    groups = defaultdict(list)
-
-    for first, *rest in lst:
-        rest = rest[0] if len(rest) == 1 else rest
-        groups[first].append(rest)
-
-    return groups
-
-
-def process_grouped_by_first_item(lst):
-    """
-        Requires items in list to already be grouped by first item.
-    """
-
-    groups = defaultdict(list)
-
-    started = False
-    last_group = None
-
-    for first, *rest in lst:
-        rest = rest[0] if len(rest) == 1 else rest
-
-        if started and first != last_group:
-            yield (last_group, groups[last_group])
-            assert first not in groups, f"{first} seen earlier --- violates precondition."
-
-        groups[first].append(rest)
-
-        last_group = first
-        started = True
-
-    return groups
-
 
 def grouper(iterable, n, fillvalue=None):
     """
@@ -457,36 +255,6 @@ def grouper(iterable, n, fillvalue=None):
 
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=fillvalue)
-
-
-# see https://stackoverflow.com/a/45187287
-class NullContextManager(object):
-    def __init__(self, dummy_resource=None):
-        self.dummy_resource = dummy_resource
-    def __enter__(self):
-        return self.dummy_resource
-    def __exit__(self, *args):
-        pass
-
-
-def load_batch_backgrounds(args, qids):
-    if args.qid2backgrounds is None:
-        return None
-
-    qbackgrounds = []
-
-    for qid in qids:
-        back = args.qid2backgrounds[qid]
-
-        if len(back) and type(back[0]) == int:
-            x = [args.collection[pid] for pid in back]
-        else:
-            x = [args.collectionX.get(pid, '') for pid in back]
-
-        x = ' [SEP] '.join(x)
-        qbackgrounds.append(x)
-    
-    return qbackgrounds
 
 
 class ColBERT(BertPreTrainedModel):
@@ -616,20 +384,11 @@ class ColBertRetrieval:
 
         with torch.no_grad() :
             colbert_encoder.eval()
-            # query (input_id, attention_mask) -> Q emb
-            # q_emb = colbert_encoder.query(q_tokenizer.tensorize(queries))
-            # q_emb = colbert_encoder.query(q_tokenizer.tensorize(queries)[0].to('cuda'), q_tokenizer.tensorize(queries)[1].to('cuda'))
-            # q_emb = q_emb.to('cpu')
-
-            ############################# 추가 ###############################
             q_embs = []
             for q in tqdm(queries) :
-                # p = tokenizer(p, padding='max_length', truncation=True, return_tensors='pt').to('cuda')
-                # p_emb = p_encoder(**p).to('cpu').numpy()
                 q_emb = colbert_encoder.doc(self.q_tokenizer.tensorize([q])[0].to('cuda'), self.q_tokenizer.tensorize([q])[1].to('cuda'))
                 q_emb = q_emb.to('cpu')
                 q_embs.append(q_emb)
-
 
         result_scores = []
         result_indices = []
@@ -711,7 +470,7 @@ class ColBertRetrieval:
                     print(f'{epoch}epoch loss: {losses/(step+1)}') # Accumulation할 경우 주석처리
 
                 self.colbert_encoder.zero_grad()
-                # self.p_encoder.zero_grad()
+
                 loss.backward()
                 optimizer.step()
                 scheduler.step()
@@ -720,7 +479,7 @@ class ColBertRetrieval:
                 
                 #torch.cuda.empty_cache()
                 del p_inputs, q_inputs
-                
+
             acc = self.evaluate(args=args, colbert_encoder=self.colbert_encoder)
             if acc > best_acc:
                 torch.save(self.colbert_encoder, "./colbert_encoder.pt")
@@ -773,7 +532,6 @@ class ColBertRetrieval:
         return correct_cnt / len(self.dataset['validation'])
 
 
-
 def main(dataset_path):
     args = TrainingArguments(
         output_dir="dense_retireval",
@@ -807,10 +565,6 @@ def main(dataset_path):
         colbert_encoder=colbert_encoder,
         df=df,
     )
-
-    # if torch.cuda.is_available():
-    #     p_encoder.cuda()
-    #     q_encoder.cuda()
 
     colbert_encoder = retriever.train()
 
